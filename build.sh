e6fd4f7d792f4daa0afa271cba000e55ab8667ce
Anemunt <69436164+darkresident55@users.noreply.github.com>
2025-12-06 18:44:15 -0500

    fixed a overriding issue with neovim/vim/lazyvim building.

--- build.sh
+++ build.sh
@@ -35,4 +35,4 @@ cmake ..
 cmake --build . -- -j"$(nproc)"
 cp -r ../Resources .
 cp Resources/imgui.ini .
-ln -s build/compile_commands.json compile_commands.json
+ln -sf build/compile_commands.json compile_commands.json
