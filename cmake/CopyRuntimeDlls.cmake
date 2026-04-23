c015f453211f2fd5e1f76860805d8e918aedff5a
Anemunt <69436164+darkresident55@users.noreply.github.com>
2026-04-23 19:00:46 -0400

    Fixed video thing hopefully due to too little DLL's being used lol

--- cmake/CopyRuntimeDlls.cmake
+++ cmake/CopyRuntimeDlls.cmake
@@ -3,7 +3,7 @@ if(NOT DEFINED DST_DIR)
 endif()
 
 if(NOT DEFINED RUNTIME_DLLS OR RUNTIME_DLLS STREQUAL "")
-    return()
+    set(RUNTIME_DLLS "")
 endif()
 
 foreach(runtime_dll IN LISTS RUNTIME_DLLS)
@@ -18,3 +18,21 @@ foreach(runtime_dll IN LISTS RUNTIME_DLLS)
         )
     endif()
 endforeach()
+
+if(DEFINED RUNTIME_DLL_DIRS AND NOT RUNTIME_DLL_DIRS STREQUAL "")
+    foreach(runtime_dll_dir IN LISTS RUNTIME_DLL_DIRS)
+        if(runtime_dll_dir STREQUAL "" OR NOT IS_DIRECTORY "${runtime_dll_dir}")
+            continue()
+        endif()
+
+        file(GLOB runtime_dir_dlls "${runtime_dll_dir}/*.dll")
+        foreach(runtime_dir_dll IN LISTS runtime_dir_dlls)
+            if(EXISTS "${runtime_dir_dll}")
+                execute_process(
+                    COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${runtime_dir_dll}" "${DST_DIR}"
+                    COMMAND_ERROR_IS_FATAL ANY
+                )
+            endif()
+        endforeach()
+    endforeach()
+endif()
